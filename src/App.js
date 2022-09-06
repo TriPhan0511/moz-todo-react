f0dbe7c85b189484eb1b25e2b0f6983fa54ce090
Tri Phan <phanphutri@gmail.com>
2022-09-06 20:37:05 +0700

    Do Lesson 03

--- src/App.js
+++ src/App.js
@@ -1,106 +1,86 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
+import Todo from './components/Todo'
+import Form from './components/Form'
+import FilterButton from './components/FilterButton'
+
 function App(props) {
-	return (
-		<div className='todoapp stack-large'>
-			<h1>TodoMatic</h1>
-
-			{/* Writing out a new task */}
-			<form>
-				<h2 className='label-wrapper'>
-					<label htmlFor='new-todo-input' className='label__lg'>
-						What needs to be done?
-					</label>
-				</h2>
-				<input
-					type='text'
-					id='new-todo-input'
-					className='input input__lg'
-					name='text'
-					autoComplete='off'
-				/>
-				<button type='submit' className='btn btn__primary btn__lg'>
-					Add
-				</button>
-			</form>
-
-			{/* Array of buttons that will be used to filter the tasks */}
-			<div className='filters btn-group stack-exception'>
-				<button type='button' className='btn toggle-btn' aria-pressed='true'>
-					<span className='visually-hidden'>Show </span>
-					<span>all</span>
-					<span className='visually-hidden'> tasks</span>
-				</button>
-				<button type='button' className='btn toggle-btn' aria-pressed='true'>
-					<span className='visually-hidden'>Show </span>
-					<span>Active</span>
-					<span className='visually-hidden'> tasks</span>
-				</button>
-				<button type='button' className='btn toggle-btn' aria-pressed='true'>
-					<span className='visually-hidden'>Show </span>
-					<span>Completed</span>
-					<span className='visually-hidden'> tasks</span>
-				</button>
-			</div>
-
-			{/* A heading that shows how many tasks remain */}
-			<h2 id='list-heading'>3 tasks remaining</h2>
-
-			{/* List of tasks */}
-			<ul
-				role='list'
-				className='todo-list stack-large stack-exception'
-				aria-labelledby='list-heading'
-			>
-				<li className='todo stack-small'>
-					<div className='c-cb'>
-						<input id='todo-0' type='checkbox' defaultChecked={true} />
-						<label className='todo-label' htmlFor='todo-0'>
-							Eat
-						</label>
-					</div>
-					<div className='btn-group'>
-						<button type='button' className='btn'>
-							Edit <span className='visually-hidden'>Eat</span>
-						</button>
-						<button type='button' className='btn btn__danger'>
-							Delete <span className='visually-hidden'>Eat</span>
-						</button>
-					</div>
-				</li>
-				<li className='todo stack-small'>
-					<div className='c-cb'>
-						<input id='todo-1' type='checkbox' />
-						<label className='todo-label' htmlFor='todo-1'>
-							Sleep
-						</label>
-					</div>
-					<div className='btn-group'>
-						<button type='button' className='btn'>
-							Edit <span className='visually-hidden'>Sleep</span>
-						</button>
-						<button type='button' className='btn btn__danger'>
-							Delete <span className='visually-hidden'>Sleep</span>
-						</button>
-					</div>
-				</li>
-				<li className='todo stack-small'>
-					<div className='c-cb'>
-						<input id='todo-2' type='checkbox' />
-						<label className='todo-label' htmlFor='todo-2'>
-							Repeat
-						</label>
-					</div>
-					<div className='btn-group'>
-						<button type='button' className='btn'>
-							Edit <span className='visually-hidden'>Repeat</span>
-						</button>
-						<button type='button' className='btn btn__danger'>
-							Delete <span className='visually-hidden'>Repeat</span>
-						</button>
-					</div>
-				</li>
-			</ul>
-		</div>
-	);
+  const taskList = props.tasks?.map((task) => (
+    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
+  ))
+  return (
+    <div className='todoapp stack-large'>
+      <h1>TodoMatic</h1>
+      <Form />
+      <div className='filters btn-group stack-exception'>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id='list-heading'>{taskList.length} tasks remaining</h2>
+      <ul
+        role='list'
+        className='todo-list stack-large stack-exception'
+        aria-labelledby='list-heading'
+      >
+        {taskList}
+      </ul>
+    </div>
+  )
 }
+// function App(props) {
+//   const taskList = props.tasks?.map((task) => (
+//     <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
+//   ))
+//   return (
+//     <div className='todoapp stack-large'>
+//       <h1>TodoMatic</h1>
 
-export default App;
+//       <form>
+//         <h2 className='label-wrapper'>
+//           <label htmlFor='new-todo-input' className='label__lg'>
+//             What needs to be done?
+//           </label>
+//         </h2>
+//         <input
+//           type='text'
+//           id='new-todo-input'
+//           className='input input__lg'
+//           name='text'
+//           autoComplete='off'
+//         />
+//         <button type='submit' className='btn btn__primary btn__lg'>
+//           Add
+//         </button>
+//       </form>
+
+//       <div className='filters btn-group stack-exception'>
+//         <button type='button' className='btn toggle-btn' aria-pressed='true'>
+//           <span className='visually-hidden'>Show </span>
+//           <span>all</span>
+//           <span className='visually-hidden'> tasks</span>
+//         </button>
+//         <button type='button' className='btn toggle-btn' aria-pressed='true'>
+//           <span className='visually-hidden'>Show </span>
+//           <span>Active</span>
+//           <span className='visually-hidden'> tasks</span>
+//         </button>
+//         <button type='button' className='btn toggle-btn' aria-pressed='true'>
+//           <span className='visually-hidden'>Show </span>
+//           <span>Completed</span>
+//           <span className='visually-hidden'> tasks</span>
+//         </button>
+//       </div>
+
+//       <h2 id='list-heading'>3 tasks remaining</h2>
+//       <ul
+//         role='list'
+//         className='todo-list stack-large stack-exception'
+//         aria-labelledby='list-heading'
+//       >
+//         {taskList}
+//       </ul>
+//     </div>
+//   )
+// }
+
+export default App
