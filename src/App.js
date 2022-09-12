4cb7dc8e259768f09f551e8cbc6c8ce94a98ae10
Tri Phan <tripp@ames.edu.vn>
2022-09-12 17:31:24 +0700

    Updated

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 /* eslint-disable jsx-a11y/no-redundant-roles */
-import Todo from './components/Todo'
-import Form from './components/Form'
-import FilterButton from './components/FilterButton'
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
 function App(props) {
-  const taskList = props.tasks?.map((task) => (
-    <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />
-  ))
+	const [tasks, setTasks] = useState(props.tasks);
 
-  function addTask(name) {
-    alert(name)
-  }
+	const taskList = tasks.map((task) => (
+		<Todo
+			id={task.id}
+			name={task.name}
+			completed={task.completed}
+			key={task.id}
+		/>
+	));
 
-  return (
-    <div className='todoapp stack-large'>
-      <h1>TodoMatic</h1>
-      <Form addTask={addTask} />
-      <div className='filters btn-group stack-exception'>
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
-      <h2 id='list-heading'>{taskList.length} tasks remaining</h2>
-      <ul
-        role='list'
-        className='todo-list stack-large stack-exception'
-        aria-labelledby='list-heading'
-      >
-        {taskList}
-      </ul>
-    </div>
-  )
+	function addTask(name) {
+		const newTask = { id: `todo-${nanoid()}`, name, completed: false };
+		setTasks([...tasks, newTask]);
+	}
+
+	const taskNoun = taskList.length !== 1 ? 'tasks' : 'task';
+	const headingText = `${taskList.length} ${taskNoun} remaining`;
+
+	return (
+		<div className='todoapp stack-large'>
+			<h1>TodoMatic</h1>
+			<Form addTask={addTask} />
+			<div className='filters btn-group stack-exception'>
+				<FilterButton />
+				<FilterButton />
+				<FilterButton />
+			</div>
+			<h2 id='list-heading'>{headingText}</h2>
+			<ul
+				role='list'
+				className='todo-list stack-large stack-exception'
+				aria-labelledby='list-heading'
+			>
+				{taskList}
+			</ul>
+		</div>
+	);
 }
-export default App
+export default App;
